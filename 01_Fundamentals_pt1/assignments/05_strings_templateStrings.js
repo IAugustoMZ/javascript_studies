0bb9aac8f335da22f0e4f1fe9a02ad5c4be6f622
Zelioli <izelioli@deloitte.com>
2024-07-26 16:13:06 -0300

    ✨feature: adds more fundamentals files eg: conditionals, strins and conversion

---
+++ 01_Fundamentals_pt1/assignments/05_strings_templateStrings.js
@@ -0,0 +1,11 @@
+// recreate the description variable using template strings
+
+const country = 'Portugal';
+const continent = 'Europe';
+const population_pt = 11;
+const language = 'portuguese';
+
+const description = `${country} is in ${continent}, and its ${population_pt} million people speak ${language}`;
+
+console.log('Assignment 5 - Template Strings ---------------------------');
+console.log(description);
